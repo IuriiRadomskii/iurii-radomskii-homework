8607c9789210f5b164099a87fc338faa18018c9a
IuriiRadomskii <uraradomsky@gmail.com>
2021-10-21 14:22:24 +0300

    rename test class, import soft assertions class

--- src/test/java/com/epam/tc/hw2/tests/ExerciseOneTest.java
+++ src/test/java/com/epam/tc/hw2/tests/ExerciseOneTest.java
@@ -1,54 +1,54 @@
 package com.epam.tc.hw2.tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import java.lang.reflect.Array;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.By;
+import java.awt.desktop.ScreenSleepEvent;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.SoftAssertions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import org.testng.asserts.Assertion;
-import org.testng.asserts.SoftAssert;
-import
 
 
-public class ExerciseOne {
+public class ExerciseOneTest {
 
     private WebDriverWait webDriverWait;
-    private WebDriver webDriver;
+    private WebDriver driver;
     private WebElement webElement;
     private final String userName = "Roman";
     private final String pass = "Jdi1234";
     private final String loggedUserName = "ROMAN IOVLEV";
     private final String pageTitle = "Home Page";
 
+
     @BeforeClass
     public void setupChromeDriver() {
         WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
     }
 
     @Test
     public void simpleTest() throws InterruptedException {
-        webDriver = new ChromeDriver();
-        webDriver.navigate().to("https://jdi-testing.github.io/jdi-light/index.html");
-        webDriver.manage().window().maximize();
-        //SoftAssert softAssertion = new SoftAssert();
-        Assertion.assertNotNull(webDriver.getTitle());
-        Assertion.assertEquals(webDriver.getTitle(), pageTitle);
-        Thread.sleep(1000);
+        driver.navigate().to("https://jdi-testing.github.io/jdi-light/index.html");
+        driver.manage().window().maximize();
+        String pageID = driver.getWindowHandle();
+        //System.out.println(pageID);
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(pageID).isNotEmpty();
+        String expectedPageTitle = driver.getTitle();
+        softly.assertThat(pageTitle).isEqualTo(expectedPageTitle);
+        //System.out.println(expectedPageTitle);
+
 
     }
 
     @AfterClass
     public void clear() {
-        webDriver.quit();
+        driver.quit();
+        driver = null;
     }
 
 }
