0f94c88791bd7f787f30330cf042dbb6d934f123
IuriiRadomskii <uraradomsky@gmail.com>
2021-10-28 16:54:28 +0300

    added IndexPage and ContactsPade classes

---
+++ src/test/java/com/epam/tc/hw3/page/IndexPage.java
@@ -0,0 +1,13 @@
+package com.epam.tc.hw3.page;
+
+import org.openqa.selenium.WebDriver;
+
+public class IndexPage {
+
+    private WebDriver driver;
+    private final String indexURL = "https://jdi-testing.github.io/jdi-light/index.html";
+
+
+
+
+}
